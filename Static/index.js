210a553df5d2e8015c9e5b149a3bc9f30435eb76
Wikonator <topes.jebal@gmail.com>
2016-09-15 17:19:49 +0200

    added a second route to a homepage, writing user input into a JSON object

--- Static/index.js
+++ Static/index.js
@@ -7,35 +7,83 @@
         Handlebars.templates[script.id] = Handlebars.compile(script.innerHTML);
     })
 
+    var RegistrationModel = Backbone.Model.extend({
+        initialize: function() {
+
+        },
+
+        url: "/register"
+    });
+
+    var HomePageModel = Backbone.Model.extend({
+        initialize: function(){
+
+        },
+
+        url: "/homepage"
+    });
+
+
+
+
+
+// window.location.hash = "registration";
+
+
+
+     var HomePageView = Backbone.View.extend({
+         initialize: function () {
+             this.render();
+             console.log("ding dong");
+         },
+         render: function () {
+             this.$el.html(Handlebars.templates.homePage())
+         },
+         el: "#main",
+     });
+
+    var registrationView = Backbone.View.extend({
+        initialize: function() {
+            this.render();
+        },
+        render: function() {
+            this.$el.html(Handlebars.templates.falls());
+
+        },
+        events: {"click #submitButton": "saveThis"
+        },
+        saveThis: function() {
+        //
+            this.model.save({
+                "usename": $('#usename').val(),
+                "pass" : $('#pass').val()
+            });
+        },
+        el: "#main",
+    })
+
 
     var Router = Backbone.Router.extend({
         routes: {
-            "/frank" : "/frank",
-            "/login" : "/login"
+            "homepage" : "homepage",
+            "registration" : "register"
+        },
+        register : function() {
+            var regModel = new RegistrationModel();
+            new registrationView({
+                model: regModel
+            });
+        },
+        homepage : function() {
+            var homeModel = new HomePageModel();
+            new HomePageView({
+                model: homeModel
+            });
         }
-    })
+    });
 
     var router = new Router();
 
     Backbone.history.start();
 
-    var UserModel = Backbone.Model.extend({
-        initialize: function() {
-        },
-            url: "/frank"
-    })
-
-     var userModel = new UserModel();
-
-    var UsersView = Backbone.View.extend({
-        initialize: function() {
-            this.render();
-        },
-        render: function() {
-            console.log("render just ran");
-            this.$el.html(Handlebars.templates.falls())
-        },
-        el: "#main"
-    })
-    new UsersView();
 })();
--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 var express = require("express"),
+    bodyParser = require("body-parser"),
     app = express();
 
 app.use(express.static(__dirname + "/Static"));
+app.use(bodyParser.json());
 
 app.get("/frank", function (req, res) {
     res.sendFile(__dirname + "/Static/index.html");
 });
 
+
+app.post("/register", function(req, res) {
+    console.log(req.body);
+});
+
+app.get("/homepage", function(req, res) {
+    console.log(req.body);
+});
 
 app.listen(9001);
 
